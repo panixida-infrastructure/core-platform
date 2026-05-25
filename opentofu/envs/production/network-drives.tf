6292350f21b958f8fc8da3e8f7cd800d59489843
Михаил <mixaila17may2003@gmail.com>
2026-05-25 23:24:17 +0400

    fix: set network drive comment

--- opentofu/envs/production/network-drives.tf
+++ opentofu/envs/production/network-drives.tf
@@ -3,7 +3,7 @@ resource "twc_network_drive" "core_platform_nvme" {
   availability_zone = "msk-1"
   preset_id         = var.network_drive_preset_id
   size              = var.network_drive_size_gb
-  comment           = ""
+  comment           = "Core platform Kubernetes retained NVMe storage."
 
   lifecycle {
     prevent_destroy = true
